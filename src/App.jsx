4a89a2f6c83a3012f4a49f0127f08fa02a93c5b0
an <anny880811@gmail.com>
2026-06-10 14:33:59 +0800

    feat: 建立喜帖基本 HTML 與 SCSS 架構

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Cover from "./sections/Cover";
+
+
+function App() {
+
+  return (
+    <>
+      <Cover />
+    </>
+  )
+}
+
+export default App
---
+++ src/sections/Cover.jsx
@@ -0,0 +1,19 @@
+const Cover = () => {
+  return (<>
+    <div className="invitation-container">
+      <div className="cover-section">
+        <h1 className="title">Beni ＆ Yiting</h1>
+        <h1 className="title">我們結婚了</h1>
+        <p>2026.10.10</p>
+      </div>
+      <div className="content-section">
+        <div>
+          <img src="" alt="" />
+        </div>
+        <p>誠摯邀請您前來...</p>
+      </div>
+    </div>
+  </>)
+}
+
+export default Cover
