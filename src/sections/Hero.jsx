b3966dc473684df1409f59ea5dc02541c4af62b8
an <anny880811@gmail.com>
2026-06-11 01:58:51 +0800

    feat:Cover、Hero UI 完成

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,16 +1,19 @@
 const Hero = () => {
   return (<>
     <div className="content-section">
-       <div className="bg-img">
-      <img src="/src/assets/01.png" className="bg-img-t" alt="背景插畫圖" />
-      <img src="/src/assets/03.png" className="bg-img-b" alt="背景插畫圖" />
-    </div>
       <div className="img-container">
         {/* <img src="/src/assets/img/37467329_cuteanimated_542.jpg" alt="婚紗照" className="main-img" /> */}
       </div>
       <h1 className="title">Beni ＆ Yiting</h1>
-      <p>2027.01.23</p>
-      <h5>婚禮場地</h5>
+      <div className="section-title">A Note From Us</div>
+      <h3 className="section-header">我們的邀請</h3>
+      <div class="section-heading__divider"></div>
+      <div className="section-content">
+        <p>感謝您願意與我們一起飛往峇里島，</p>
+      <p>見證人生中最重要的一天。</p>
+      <p>這不只是婚禮，</p>
+      <p>更是一趟屬於我們的旅行</p>
+      </div>
     </div>
   </>)
 }
