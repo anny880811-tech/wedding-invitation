e335d0fdabd9859589f11a1a2959c8bc9f1fd005
an <anny880811@gmail.com>
2026-06-18 01:39:16 +0800

    feat: leaveMessage UI 完成

--- src/sections/LeaveMessage.jsx
+++ src/sections/LeaveMessage.jsx
@@ -6,15 +6,28 @@ const LeaveMessage = () => {
       <div className="section-heading__divider"></div>
       <div className="wish-container">
         <div>
-          <h5>100則</h5>
+          <h5>100則留言</h5>
         </div>
         <div className="input-group">
           <input type="text" name="" id="" className="name" placeholder="名字 Name" />
-          <input type="text" name="" id="" className="wishes" placeholder="給我們的祝福 Wishes" />
+          <textarea type="text" name="" id="" className="wishes" placeholder="給我們的祝福 Wishes" />
         </div>
-
-        <button type="submit">送出</button>
+        <button type="submit" className="wish-btn">送出 Send</button>
+        {/* <div>
+          <div className="message-line"></div>
+          <div className="message-board">
+            <p className="title">安安</p>
+            <p>恭喜恭喜~~，新婚愉快</p>
+          </div>
+        </div> */}
       </div>
+       <div>
+          <div className="message-board">
+            <p className="title">安安</p>
+            <p>恭喜恭喜~~，新婚愉快</p>
+            <div className="message-line"></div>
+          </div>
+        </div>
     </div>
   </>)
 }
