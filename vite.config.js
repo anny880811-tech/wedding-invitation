d9ea31153737748524d66dc7fa0aa547596548dd
an <anny880811@gmail.com>
2026-06-26 01:14:23 +0800

    fix: config 修改

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,8 @@
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   base: '/',
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://wedding-invitation-self-beta.vercel.app/',
-        changeOrigin: true,
-      }
-    }
-  }
 })
